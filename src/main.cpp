f3aaef26216a8990967b1c2bd589e374f05c26c4
Alexandria P. <alxpettit@gmail.com>
2024-11-14 21:14:29 -0700

    autoformat with clang-format

--- src/main.cpp
+++ src/main.cpp
@@ -21,30 +21,30 @@ int PIN_HALL_INDEX = 11;
 
 BLDCMotor motor = BLDCMotor(PP, R, KV, L);
 
-// Uses ACS712. Value docs use is 66.0 mVpa. Source: https://docs.simplefoc.com/inline_current_sense
-InlineCurrentSense current_sense = InlineCurrentSense(66.0, CURRENT_SENSE_1, _NC, CURRENT_SENSE_3);
-
+// Uses ACS712. Value docs use is 66.0 mVpa. Source:
+// https://docs.simplefoc.com/inline_current_sense
+InlineCurrentSense current_sense =
+    InlineCurrentSense(66.0, CURRENT_SENSE_1, _NC, CURRENT_SENSE_3);
 
 // init driver
 BLDCDriver3PWM driver = BLDCDriver3PWM(PIN_A, PIN_B, PIN_C, PIN_ENABLE);
 //  init encoder
 Encoder encoder = Encoder(PIN_HALL_A, PIN_HALL_B, 2048);
 // channel A and B callbacks
-void doA(){encoder.handleA();}
-void doB(){encoder.handleB();}
+void doA() { encoder.handleA(); }
+void doB() { encoder.handleB(); }
 
 // angle set point variable
 float target_angle = 0;
 // commander interface
 Commander command = Commander(Serial);
-void onTarget(char* cmd){ command.scalar(&target_angle, cmd); }
+void onTarget(char *cmd) { command.scalar(&target_angle, cmd); }
 
 void setup() {
   // monitoring port
   Serial.begin(115200);
   SimpleFOCDebug::enable();
   motor.useMonitoring(Serial);
-
 
   // initialize encoder hardware
   encoder.init();
@@ -61,19 +61,20 @@ void setup() {
   driver.voltage_power_supply = 20;
 
   // note: current_sense.init() must be AFTER driver.init()
-  if (current_sense.init())  Serial.println("Current sense init success!");
+  if (current_sense.init())
+    Serial.println("Current sense init success!");
   else {
     Serial.println("Current sense init failed!");
     return;
   }
 
   current_sense.linkDriver(&driver);
-  
+
   motor.linkDriver(&driver);
 
   // set control loop to be used
   motor.controller = MotionControlType::angle;
-  // controller configuration based on the control type 
+  // controller configuration based on the control type
   // velocity PI controller parameters
   // default P=0.5 I = 10
   // motor.PID_velocity.P = 0.2;
@@ -81,24 +82,24 @@ void setup() {
   // jerk control using voltage voltage ramp
   // default value is 300 volts per sec  ~ 0.3V per millisecond
   // motor.PID_velocity.output_ramp = 1000;
-  
-  //default voltage_power_supply
+
+  // default voltage_power_supply
   motor.voltage_limit = 2;
 
   // velocity low pass filtering
-  // default 5ms - try different values to see what is the best. 
+  // default 5ms - try different values to see what is the best.
   // the lower the less filtered
   // motor.LPF_velocity.Tf = 0.01;
   motor.LPF_velocity.Tf = 0.1;
 
-  // angle P controller 
+  // angle P controller
   // default P=20
   motor.P_angle.P = 20;
   //  maximal velocity of the position control
   // default 20
   // motor.velocity_limit = 4;
   motor.velocity_limit = 20;
-  
+
   // initialize motor
   motor.init();
   // align encoder and start FOC
@@ -117,10 +118,10 @@ void loop() {
   // iterative FOC function
   motor.loopFOC();
 
-  // function calculating the outer position loop and setting the target position 
+  // function calculating the outer position loop and setting the target
+  // position
   motor.move(target_angle);
 
   // user communication
   command.run();
 }
-
