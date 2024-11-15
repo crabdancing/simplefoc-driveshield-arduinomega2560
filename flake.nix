ee645eee80165b0e7076807ee7cdef290a29dad0
Alexandria P. <alxpettit@gmail.com>
2024-11-14 17:28:09 -0700

    rewrite flake.nix using flake-parts std, and rebuild platformio2nix.lock

--- flake.nix
+++ flake.nix
@@ -2,29 +2,40 @@
   description = "simplefoc-driveshield-mega2560";
 
   inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     platformio2nix.url = "github:nathanregner/platformio2nix";
-    nixpkgs.url = "github:Nixos/nixpkgs/nixos-unstable";
   };
 
-  outputs = {platformio2nix, ...}: let
-    nixpkgs = platformio2nix.inputs.nixpkgs;
-    inherit (nixpkgs) lib;
-    forAllSystems = lib.genAttrs [
-      "aarch64-darwin"
-      "aarch64-linux"
-      "x86_64-darwin"
-      "x86_64-linux"
-    ];
-  in {
-    packages = forAllSystems (
-      system: let
-        pkgs = import nixpkgs {
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        # To import a flake module
+        # 1. Add foo to inputs
+        # 2. Add foo as a parameter to the outputs function
+        # 3. Add here: foo.flakeModule
+
+      ];
+      systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        # Per-system attributes can be defined here. The self' and inputs'
+        # module parameters provide easy access to attributes of the same
+        # system.
+
+        # Equivalent to  inputs'.nixpkgs.legacyPackages.hello;
+        _module.args.pkgs = import inputs.nixpkgs {
           inherit system;
-          overlays = [platformio2nix.overlays.default];
+          overlays = [inputs.platformio2nix.overlays.default];
         };
-      in {
-        default = pkgs.callPackage ./package.nix {};
-      }
-    );
-  };
+        # packages.default = pkgs.hello;
+        packages.default = pkgs.callPackage ./package.nix {};
+
+      };
+      flake = {
+        # The usual flake attributes can be defined here, including system-
+        # agnostic ones like nixosModule and system-enumerating ones, although
+        # those are more easily expressed in perSystem.
+
+      };
+    };
 }
