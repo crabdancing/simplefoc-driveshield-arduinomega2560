638b68b78df2ac7c9dda5606f2c44eddd056b860
Alexandria P. <alxpettit@gmail.com>
2024-11-14 23:14:12 -0700

    current sense actually works now?

--- src/main.cpp
+++ src/main.cpp
@@ -23,8 +23,9 @@ BLDCMotor motor = BLDCMotor(PP, R, KV, L);
 
 // Uses ACS712. Value docs use is 66.0 mVpa. Source:
 // https://docs.simplefoc.com/inline_current_sense
+// antun says scale value from 3.3v range to 5v range
 InlineCurrentSense current_sense =
-    InlineCurrentSense(66.0, CURRENT_SENSE_1, _NC, CURRENT_SENSE_3);
+    InlineCurrentSense(66.0 * (3.3 / 5), CURRENT_SENSE_1, _NC, CURRENT_SENSE_3);
 
 // init driver
 BLDCDriver3PWM driver = BLDCDriver3PWM(PIN_A, PIN_B, PIN_C, PIN_ENABLE);
@@ -86,7 +87,7 @@ void setup() {
   // motor.PID_velocity.output_ramp = 1000;
 
   // default voltage_power_supply
-  motor.voltage_limit = 1;
+  motor.voltage_limit = 10;
 
   // velocity low pass filtering
   // default 5ms - try different values to see what is the best.
